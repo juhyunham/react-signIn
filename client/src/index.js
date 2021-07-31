58005d3d599134d51748b510821b0a8b9f2927af
juhyunham <judev0227@gmail.com>
2021-07-31 23:04:37 +0900

    update reducer

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducer'
 
-import 'antd/dist/antd.css'
+const createStoreWidthMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(ReduxThunk)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider 
+      store= {createStoreWidthMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && 
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
